cecb66842d32be06b44bc0b7010076a509f49c65
acherianus <acherianus@gmail.com>
2018-01-30 22:12:02 -0500

    Add get /todos/id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
 var {  mongoose} = require('./db/mongoose');
 
 var {Todo} = require('./models/todo');
@@ -28,6 +29,25 @@ app.get('/todos', (req,res) => {
   }, (e) => {
     res.status(400).send(e);
   })
+});
+
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (todo) {
+      return res.status(200).send({todo});
+    } else {
+      return res.status(404).send();
+    }
+  }, (e) => {
+    return res.status(400).send();
+  });
+
 });
 
 app.listen(3000, () => {
