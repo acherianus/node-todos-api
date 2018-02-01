789a43c3a76942d871bbae049bee9187151beaca
acherianus <acherianus@gmail.com>
2018-01-31 21:48:01 -0500

    add delete todos

--- server/server.js
+++ server/server.js
@@ -51,6 +51,22 @@ app.get('/todos/:id', (req,res) => {
 
 });
 
+app.delete('/todos/:id',(req,res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((doc) => {
+    if (doc) {
+      return res.status(200).send({doc});
+    }
+    return res.status(404).send();
+  }, (e) => {
+    return res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
