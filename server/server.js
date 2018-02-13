7ed2d7827c9e20ae52f58e407869b3ac38d8e04c
acherianus <acherianus@gmail.com>
2018-02-12 22:37:33 -0500

    Add POST /users and token method

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var {ObjectID} = require('mongodb');
-var {  mongoose} = require('./db/mongoose');
+const {ObjectID} = require('mongodb');
+const {  mongoose} = require('./db/mongoose');
 
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -21,6 +22,31 @@ app.post('/todos', (req,res) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
+  })
+});
+
+app.patch('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text','completed']);
+
+  if (ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set:{body}},{new:true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   })
 });
 
@@ -64,6 +90,19 @@ app.delete('/todos/:id',(req,res) => {
     return res.status(404).send();
   }, (e) => {
     return res.status(400).send();
+  });
+});
+
+app.post('/users', (req,res) => {
+  var body = _.pick(req.body, ['email','password']);
+  var user = new User(body);
+  user.save().then((user) => {
+    return user.generateAuthToken();
+    //res.send(user);
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((err) => {
+    res.status(400).send(err);
   });
 });
 
